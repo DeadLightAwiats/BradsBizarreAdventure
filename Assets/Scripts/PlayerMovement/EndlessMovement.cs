b7f2993db0cef5fd712b068c169bfdaf1dc16df4
JaySyk <jaydensykes989@gmail.com>
2025-06-09 13:37:49 +0100

    Fix Scene being missing

---
+++ Assets/Scripts/PlayerMovement/EndlessMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EndlessMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float moveSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveDirection = Input.GetAxisRaw("Vertical");
+        rb.velocity =new Vector2(0, moveDirection * moveSpeed);
+    }
+}
